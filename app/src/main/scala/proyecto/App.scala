3bf1b37bec38bb1f1410b6eb1c25932e543f7425
Andros-Q <andrespipe080406@gmail.com>
2026-06-05 14:44:40 -0500

    He realizado la funcion capacidad fallida con sus 5 casos de prueba .esta es una funcion del ejercicio 2.5

--- app/src/main/scala/proyecto/App.scala
+++ app/src/main/scala/proyecto/App.scala
@@ -4,5 +4,7 @@ object App {
   def main(args: Array[String]): Unit = {
     println("Proyecto Final - Asignación Óptima de Aulas")
     println("Implemente AsignacionAulas y AsignacionAulasPar para comenzar.")
+
+
   }
 }
